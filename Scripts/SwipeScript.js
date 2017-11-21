188ea24e9997196c365c7463a5d8599f364a0935
NChatlosh <nicholas.chatlosh@loop.colum.edu>
2017-11-20 19:17:26 -0600

    Add files via upload

---
+++ Scripts/SwipeScript.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+
+    //$("p").on("swipeleft", function () {
+    //alert("Swipe left event detected!");
+    //});
+
+    $( document ).on("swipe", swipeHandler);
+    // $("#tabSwipe").swipe(swipeHandler);
+    function swipeHandler(event) {
+        $(event.target).addClass("swipe");
+        $alert("Swipe right event detected!");
+        $("#sectionA").hide();
+        $("#sectionB").show();
+    }
+
+});
