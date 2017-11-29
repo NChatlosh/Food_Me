4fd214c8eabf79b733b9b2b8fddb768681c22915
NChatlosh <nicholas.chatlosh@loop.colum.edu>
2017-11-29 08:49:27 -0600

    Add files via upload

---
+++ Scripts/Vars.js
@@ -0,0 +1,30 @@
+var key = '5df20780c96bdedadcce283a9c2ac048';
+var EstabPath = "\JSON_Files/Establishments/";
+var RegionPath = "\JSON_Files/Regions/";
+var DineMethURL = "\JSON_Files/Dine_Method/Dine_Method.txt";
+var MealTypeURL = "\JSON_Files/Meal_Type/Meal_Type.txt";
+
+var globalrestObject = null;
+var Budget = 5;
+
+var EstabList = new Array();
+var RegionList = new Array();
+
+var dmValue;
+var budgetValue;
+var vegValue;
+var mtValue;
+
+var event = new CustomEvent("Ready");
+var eventFired = false;
+
+var listsCount = 0;
+var listsCountAfterLoad = 0;
+
+var nothingChecked = true;
+
+var userLong = 41.945014;
+var userLat = -87.667741;
+
+var gotLoc = false;
+var searchRadius = 3212 //2 miles
